0cf6e88f217b1c42cb4094510d8add66602af856
chuhojin <chuhojin2@gmail.com>
2016-06-27 17:09:54 +0900

    헬로우 자바

---
+++ src/HelloJava.java
@@ -0,0 +1,30 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello JAVA World");
+		/*System.out.println("Hello JAVA World");
+		System.out.println("Hello JAVA World");*/
+		System.out.println("Hello JAVA World");
+		System.out.println("Hello JAVA World");
+		
+		/*
+		 * 화면에 내용 출력
+		 * System.out.println();
+		 * 		line feed의 약자, 행을 넘김
+		 * System.out.print();
+		 * System.out.printf();
+		 * 		format 약자, 서식을 지정한다는 의미.
+		 */
+	}
+
+}
+
+
+
+
+
+
+
+
+
